c69233d58ff6080b6c0303dc7097ee5fc0a02dbc
Gaby Nieva <gaby.nieva@outlook.com>
2023-10-09 12:53:11 -0600

    Adding method to count the size of the file

--- src/ManipulacionArchivos.java
+++ src/ManipulacionArchivos.java
@@ -6,7 +6,29 @@ import java.io.InputStreamReader;
 
 public class ManipulacionArchivos {
 
+    public static int contarLineasArchivo(String name){
+      File archivo; // Apunta a un archivo físico del dd
+      FileReader reader; // Llave con permiso de solo lectura
+      BufferedReader bufer; // Recuperar info. del archivo
+      String linea;  
+      int numLineas = 0;
 
+      try{
+            archivo = new File("D:\\archivos\\" + name + ".txt");
+            reader = new FileReader(archivo);
+            bufer = new BufferedReader(reader);
+            // Contar las líneas que contiene el archivo
+            while ( (linea = bufer.readLine())!= null ){
+                numLineas++;
+            }
+            reader.close();
+        } catch( Exception e ){
+            System.out.println("Error al leer el archivo: "
+            + e.toString());
+        }
+        return numLineas;
+
+    }
 
     public static void leerArchivo(String name){
         File archivo; // Apunta a un archivo físico del dd
@@ -38,10 +60,13 @@ public class ManipulacionArchivos {
         BufferedReader bufer = new BufferedReader(
             new InputStreamReader(System.in));
         String fileName;
+        int t;
 
         System.out.println("Lectura de un archivo de texto");
         System.out.println("Escribe el nombre del archivo: ");
         fileName = bufer.readLine();
         leerArchivo(fileName);
+        t = contarLineasArchivo(fileName);
+        System.out.println("Líneas en el archivo " + fileName + ": " + t);
     }
 }
