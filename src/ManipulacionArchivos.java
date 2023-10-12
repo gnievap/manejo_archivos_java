6678c7dea323e8bc2e5b95f7e768098de10200a6
Gaby Nieva <gaby.nieva@outlook.com>
2023-10-12 12:23:50 -0600

    Using the files package

--- src/ManipulacionArchivos.java
+++ src/ManipulacionArchivos.java
@@ -1,16 +1,13 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+
+import files.Files;
 
 public class ManipulacionArchivos {
 
-    
-
     public static void main(String[] args) throws IOException  {
+        Files files = new Files();
         BufferedReader bufer = new BufferedReader(
             new InputStreamReader(System.in));
         String fileName;
@@ -21,7 +18,7 @@ public class ManipulacionArchivos {
         System.out.println("Lectura de un archivo de texto");
         System.out.println("Escribe el nombre del archivo: ");
         fileName = bufer.readLine();
-        mascotas = fileToStringArray(fileName);
+        mascotas = files.fileToStringArray(fileName);
         System.out.println("Contenido del arreglo de mascotas:");
         for ( String unaMascota : mascotas ){
             System.out.println(unaMascota);
@@ -30,7 +27,7 @@ public class ManipulacionArchivos {
         System.out.println("Lectura de datos numéricos");
         System.out.println("Escribe el nombre del archivo: ");
         fileName = bufer.readLine();
-        numeros = fileToIntArray(fileName);
+        numeros = files.fileToIntArray(fileName);
         System.out.println("Contenido del arreglo de numeros: ");
         for ( int unNumero : numeros ){
             System.out.println(unNumero);
@@ -44,7 +41,7 @@ public class ManipulacionArchivos {
         }
         System.out.println("Escribe el nombre del archivo de numerosX3: ");
         fileName = bufer.readLine();
-        writeArrayToFile(fileName, numerosX3);
+        files.writeArrayToFile(fileName, numerosX3);
 
 
         // System.out.println("Crear un archivo de texto");
